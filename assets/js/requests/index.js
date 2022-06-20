9dc6c32d57e26e67237d758e96a228e1258178ea
GiannPogi <gcmediavillo@gmail.com>
2022-06-20 17:09:42 +0800

    backend for file upload added

--- assets/js/requests/index.js
+++ assets/js/requests/index.js
@@ -5,55 +5,60 @@ var requestForm = document.getElementById("form-account-request")
 var office = document.getElementById("office")
 var service = document.getElementById("service")
 var userNote = document.getElementById("user_note")
-var objFile = document.getElementById("obj_file")
+var file = document.getElementById("obj_file")
+var port = 44310
+
+//OnStart
+getOffice()
+getService()
+// getRequest()
 
 // Form Submit
 requestForm.addEventListener("submit", (e) => {
 	e.preventDefault()
 
-	var values = {
-		OfficeId: office.value,
-		ServiceId: service.value,
-		UserNote: userNote.value,
-		ObjFile: objFile.files,
-	}
+	var reader = new FileReader();
+	reader.readAsDataURL(file.files[0]);
 
-	console.log(values)
+	reader.onload = function () {
+		var values = {
+			OfficeId: office.value,
+			ServiceId: service.value,
+			UserNote: userNote.value,
+			FileData: reader.result.split(",")[1]
+		}
+
+		// testBtn.href = reader.result
+		// testBtn.download = "test.pdf"
+
+		console.log(values)
+		sendRequest(values)
+	};
 })
 
-// Appending
-appendOfficeOption(office, [
-	{
-		Id: 1,
-		Name: "Office 1"
-	},
-	{
-		Id: 2,
-		Name: "Office 2"
-	}
-])
-
-appendServiceOption(service, [
-	{
-		Id: 1,
-		Name: "Service 1",
-		Fee: 20
-	},
-	{
-		Id: 2,
-		Name: "Service 2",
-		Fee: 20
-	}
-])
 
 // API Request
-async function getApi(url) {
-	const response = await fetch(url);
+async function getOffice() {
+	const response = await fetch(`https://localhost:${port}/api/request/get-all-offices`);
 	var data = await response.json();
-	return data;
+	appendOfficeOption(office, data)
 }
 
-async function sendRequest(url, values) {
+async function getService() {
+	const response = await fetch(`https://localhost:${port}/api/request/get-all-services`);
+	var data = await response.json();
+	appendServiceOption(service, data)
+}
+
+async function getRequest() {
+	const response = await fetch(`https://localhost:${port}/api/request/get-all-requests`);
+	var data = await response.json();
+	console.log(data)
+}
+
+async function sendRequest(values) {
+	var url = `https://localhost:${port}/api/request/create-request`
+
 	const response = await fetch(url, {
 		method: "POST",
 		headers: { 'Content-Type': 'application/json' },
@@ -61,13 +66,4 @@ async function sendRequest(url, values) {
 	});
 
 	console.log(response)
-
-	// fetch("https://localhost:44310/api/test/create-test", {
-	// 	method: "POST",
-	// 	headers: { 'Content-Type': 'application/json' },
-	// 	body: JSON.stringify(values)
-	// }).then(res => {
-	// 	console.log("Request complete! response:", res);
-
-	// });
 }
