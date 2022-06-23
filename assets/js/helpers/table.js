a0ed6781b0961b0ac86b121d804f4128a57216b6
Rojhon <rojhonbuenaventura5@gmail.com>
2022-06-23 15:18:39 +0800

    Fix Search

--- assets/js/helpers/table.js
+++ assets/js/helpers/table.js
@@ -1,10 +1,14 @@
 export class Table {
     static search(state, data) {
         var result = []
+        var filter = data.toString().toUpperCase()
+
+        if(data == "")
+            return state
 
         for (var i = 0; i < state.length; i++) {
             for (var key in state[i]) {
-                if (state[i][key] == data) {
+                if (state[i][key].toString().toUpperCase().indexOf(filter) > -1) {
                     result.push(state[i])
                 }
             }
--- assets/js/test.js
+++ assets/js/test.js
@@ -1,9 +1,9 @@
 import { Table } from "./helpers/table";
 import { request } from "../data/request"
 
-console.log(Table.search(request, "Pending"))
-console.log(Table.search(request, "Hi"))
+console.log(Table.search(request, "P"))
+console.log(Table.search(request, ""))
 
-console.log(Table.select(request, "Status", "Pending"))
-console.log(Table.select(request, "Status", "Processing"))
-console.log(Table.select(request, "Status", "Paid"))
+// console.log(Table.select(request, "Status", "Pending"))
+// console.log(Table.select(request, "Status", "Processing"))
+// console.log(Table.select(request, "Status", "Paid"))
