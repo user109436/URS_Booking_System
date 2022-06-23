2e9600a36972da91d255f202161cbaba61e96f82
Rojhon <rojhonbuenaventura5@gmail.com>
2022-06-23 15:19:57 +0800

    Fix Search

--- assets/js/test.js
+++ assets/js/test.js
@@ -4,6 +4,6 @@ import { request } from "../data/request"
 console.log(Table.search(request, "P"))
 console.log(Table.search(request, ""))
 
-// console.log(Table.select(request, "Status", "Pending"))
-// console.log(Table.select(request, "Status", "Processing"))
-// console.log(Table.select(request, "Status", "Paid"))
+console.log(Table.select(request, "Status", "Pending"))
+console.log(Table.select(request, "Status", "Processing"))
+console.log(Table.select(request, "Status", "Paid"))
