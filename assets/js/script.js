6dcca42bf200b79d5f42d551488d943a7191cd62
Rhotrix Loreto <rhotrix2000@gmail.com>
2022-06-24 22:24:27 +0800

    Fixed the error in using the javascript file

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+const modal = document.querySelector('#modalRegistration');
+const modalBtn = document.getElementById('btnRegister');
+const closeBtn = document.querySelector('#cancelButton');
+
+// Events
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+// Open
+function openModal() {
+  modal.style.display = 'block';
+}
+
+// Close
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+// Close If Outside Click
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = 'none';
+  }
+}
