743a9e38c347416858d55b7da53ad3e6639debe2
user109436 <zedekfelix7@gmail.com>
2022-06-25 12:44:51 +0800

    BUGFIX:undefined

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,29 @@
-const modal = document.querySelector('#modalRegistration');
-const modalBtn = document.getElementById('btnRegister');
-const closeBtn = document.querySelector('#cancelButton');
+const modal = document.querySelector("#modalRegistration");
+const modalBtn = document.getElementById("btnRegister");
+const closeBtn = document.querySelector("#cancelButton");
 
 // Events
-modalBtn.addEventListener('click', openModal);
-closeBtn.addEventListener('click', closeModal);
-window.addEventListener('click', outsideClick);
+if (modalBtn) {
+  modalBtn.addEventListener("click", openModal);
+}
+if (closeBtn) {
+  closeBtn.addEventListener("click", closeModal);
+}
+window.addEventListener("click", outsideClick);
 
 // Open
 function openModal() {
-  modal.style.display = 'block';
+  modal.style.display = "block";
 }
 
 // Close
 function closeModal() {
-  modal.style.display = 'none';
+  modal.style.display = "none";
 }
 
 // Close If Outside Click
 function outsideClick(e) {
   if (e.target == modal) {
-    modal.style.display = 'none';
+    modal.style.display = "none";
   }
 }
