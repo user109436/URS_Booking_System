22fccdb5dff9fe1496d21cb567e00a7ac25c04b1
RiYan Carl <102679900+RiYanCarl@users.noreply.github.com>
2022-06-27 20:17:20 +0800

    update 1.3
    Create new button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 import { Modal } from "./modal.class.js";
 
 
-
 // // TRACKING MODAL
 const trackModal = document.getElementById("openTrackModal");
 const trackOpenBtn = document.getElementsByClassName("modalTrack-btn");
@@ -13,8 +12,9 @@ if (trackingModalExist) {
 }
 
 
-// updated VARIABLE NAME - later
 // // UPDATE MODAL
+
+// // Registrar MODAL
 const updateModal = document.getElementById("openUpdateModal");
 const updateOpenBtn = document.getElementsByClassName("modalUpdate-btn");
 const updateCloseBtn = document.getElementById("closeUpdateModal");
@@ -25,7 +25,7 @@ if (updateModalExist) {
 }
 
 // delete when the table have thier own FILE
-// // OFFICE MODAL 
+// // OFFICE Update MODAL 
 const officeModal = document.getElementById("openOfficeModal");
 const officeOpenBtn = document.getElementsByClassName("modalOffice-btn");
 const officeCloseBtn = document.getElementById("closeOfficeModal");
@@ -35,7 +35,7 @@ if (officeModalExist) {
   tableModal.modalMultipleOpenBtn();
 }
 
-// // SERVICES MODAL 
+// // SERVICES update MODAL 
 const servicesModal = document.getElementById("openServicesModal");
 const servicesOpenBtn = document.getElementsByClassName("modalServices-btn");
 const servicesCloseBtn = document.getElementById("closeServicesModal");
@@ -45,28 +45,69 @@ if (servicesModalExist) {
   tableModal.modalMultipleOpenBtn();
 }
 
-// // ROLE MODAL 
+// // ROLE update MODAL 
 const roleModal = document.getElementById("openRoleModal");
 const roleOpenBtn = document.getElementsByClassName("modalRole-btn");
 const roleCloseBtn = document.getElementById("closeRoleModal");
 const roleModalExist = roleModal && roleOpenBtn && roleCloseBtn;
 if (roleModalExist) {
   const tableModal = new Modal(roleModal, roleOpenBtn, roleCloseBtn);
-  tableModal.modalMultipleOpenBtn();
- 
-  
+  tableModal.modalMultipleOpenBtn(); 
 }
-// // TRACKING MODAL
-// const trackModal = document.getElementById("openTrackModal");
-// const trackOpenBtn = document.getElementById("modalTrack-btn");
-// const trackCloseBtn = document.getElementById("closeTrackModal");
-// const trackingModalExist = trackModal && trackOpenBtn && trackCloseBtn;
-// if (trackingModalExist) {
-//   const trackingModalObj = new Modal(trackModal, trackOpenBtn, trackCloseBtn);
-//   trackingModalObj.modalSingleOpenBtn();
-// }
 
-// User's table Create new
+//user's table update Modal
+const userUpdateModal = document.getElementById("userUpdateModal");
+const userUpdateOpenBtn = document.getElementsByClassName("userUpdateModal-btn");
+const userCloseUpdateModal = document.getElementById("userCloseUpdateModal");
+const userUpdateModalExist = userUpdateOpenBtn && userUpdateOpenBtn && userCloseUpdateModal;
+if (userUpdateModalExist) {
+  const tableModal = new Modal(userUpdateModal, userUpdateOpenBtn, userCloseUpdateModal);
+  tableModal.modalMultipleOpenBtn();
+}
+
+// // Create MODAL -------------------------------------------------------
+
+// Office create new Modal
+const createOfficeModal = document.getElementById("openCreateOfficeModal");
+const createOfficeOpenBtn = document.getElementsByClassName("createOffice-btn");
+const createOfficeCloseBtn = document.getElementById("closeCreateOfficeModal");
+const createOfficeModalExist = createOfficeModal && createOfficeOpenBtn && createOfficeCloseBtn;
+if (createOfficeModalExist) {
+  const tableModal = new Modal(createOfficeModal, createOfficeOpenBtn, createOfficeCloseBtn);
+  tableModal.modalMultipleOpenBtn();
+}
+
+// // SERVICES create new Modal 
+const createServicesModal = document.getElementById("openCreateServicesModal");
+const createServicesOpenBtn = document.getElementsByClassName("createServices-btn");
+const createServicesCloseBtn = document.getElementById("closeCreateServicesModal");
+const createServicesModalExist = createServicesModal && createServicesOpenBtn && createServicesCloseBtn;
+if (createServicesModalExist) {
+  const tableModal = new Modal(createServicesModal, createServicesOpenBtn, createServicesCloseBtn);
+  tableModal.modalMultipleOpenBtn();
+}
+
+// // ROLE create new Modal 
+const createRoleModal = document.getElementById("openCreateRoleModal");
+const createRoleOpenBtn = document.getElementsByClassName("createRole-btn");
+const createRoleCloseBtn = document.getElementById("closeCreateRoleModal");
+const createRoleModalExist = createRoleModal && createRoleOpenBtn && createRoleCloseBtn;
+if (createRoleModalExist) {
+  const tableModal = new Modal(createRoleModal, createRoleOpenBtn, createRoleCloseBtn);
+  tableModal.modalMultipleOpenBtn(); 
+}
+
+// // REQUEST create new Modal 
+const createRequestModal = document.getElementById("openCreateTrackModal");
+const createRequestOpenBtn = document.getElementsByClassName("createRequest-btn");
+const createRequestCloseBtn = document.getElementById("closeCreateTrackModal");
+const createRequestModalExist = createRequestModal && createRequestOpenBtn && createRequestCloseBtn;
+if (createRequestModalExist) {
+  const tableModal = new Modal(createRequestModal, createRequestOpenBtn, createRequestCloseBtn);
+  tableModal.modalMultipleOpenBtn(); 
+}
+
+// User's table Create new MODAL
 const newUserModal = document.getElementById("newUserModal");
 const newUserBtn = document.getElementById("newUserBtn");
 const cancelCreateBtn = document.getElementById("cancelCreateBtn");
@@ -74,10 +115,9 @@ const userModalExist = newUserModal && newUserBtn && cancelCreateBtn
 if (userModalExist) {
   const userModalObj = new Modal(newUserModal , newUserBtn , cancelCreateBtn);
   userModalObj.modalSingleOpenBtn();
-
-
 }
 
+// DOM ----------------------------------------------------------
 
 const userForm = document.querySelector('#formAddUser');
 const inputId = document.querySelector('#newInputId');
@@ -92,25 +132,44 @@ const inputVerified = document.querySelector('#newInputVerified');
 const inputStatus = document.querySelector('#newInputStatus');
 const inputCreated = document.querySelector('#newInputCreated');
 const inputUpdated = document.querySelector('#newInputUpdated');
+
 const submitBtn = document.getElementById('createBtn').addEventListener('click', function(){
   
 
 });
 
 
+// DOM for Table Update
+
+// // Registrar DOM 
+// const inputUserNote = document.querySelector('#user_note');
+// const inputStatus = document.querySelector('#status');
 
 
-//user's table update Modal
-const userUpdateModal = document.getElementById("userUpdateModal");
-const userUpdateOpenBtn = document.getElementsByClassName("userUpdateModal-btn");
-const userCloseUpdateModal = document.getElementById("userCloseUpdateModal");
-const userUpdateModalExist = userUpdateOpenBtn && userUpdateOpenBtn && userCloseUpdateModal;
-if (userUpdateModalExist) {
-  const tableModal = new Modal(userUpdateModal, userUpdateOpenBtn, userCloseUpdateModal);
-  tableModal.modalMultipleOpenBtn();
-}
+// FormData.addEventListener('submit' onsubmit);
+// // // OOFICE DOM 
+// const inputOffice= document.querySelector('#office');
 
-// MAKE REQUEST Scroll on Trigger "a"
+
+// // // SERVICES DOM 
+// const inputServices= document.querySelector('#services');
+// const inputFee= document.querySelector('#fee');
+
+
+// // // ROLES DOM 
+// const inputRole= document.querySelector('#role');
+
+
+// // // REQUEST DOM 
+// const inputOffice= document.querySelector('#office');
+// const inputServices= document.querySelector('#services');
+// const inputUserNote= document.querySelector('#user_note');
+// const inputOfficeNote= document.querySelector('#office_note');
+
+
+
+
+// MAKE REQUEST Scroll on Trigger "a" -----------------------------------------
 
 const removeString = (originalString, stringToRemove, startLocation = 0) => {
   let stringLocation = originalString.indexOf(stringToRemove);
@@ -131,8 +190,3 @@ if (makeRequest) {
     window.location.assign(`${url}#form-account-request`);
   });
 }
-
-
-
-
-
