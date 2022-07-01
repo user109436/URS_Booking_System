2eb9307546e55af7977a77e815e0d2ecca54c58b
RiYan Carl <102679900+RiYanCarl@users.noreply.github.com>
2022-07-02 04:58:01 +0800

    update 1.5.2
    try putting loading while fetching data. not yet working

--- assets/js/dom-table.js
+++ assets/js/dom-table.js
@@ -8,8 +8,39 @@ const tableHeader = `<tr class="bg-primary" id="table-header">
                     </tr>
                     `
 console.log(tableHeader);
+const loader = document.querySelector("#loading");
+// function loader() {
+//    `<div class="loading-screen border-rounded">
+//       <div class="loading-screen-box">
+//         <div class="loading">
+//           <div class="dot">L</div>
+//           <div class="dot">O</div>
+//           <div class="dot">A</div>
+//           <div class="dot">D</div>
+//           <div class="dot">I</div>
+//           <div class="dot">N</div>
+//           <div class="dot">G</div>
+//           <span class="text">Please Wait...</span>
+//         </div>
+//       </div>
+//     </div>`;
+//   }
+  
+  function displayLoading() {
+    loader.classList.add("display");
+    // to stop loading after some time
+    setTimeout(() => {
+        loader.classList.remove("display");
+    }, 5000);
+}
+
+// hiding loading 
+function hideLoading() {
+    loader.classList.remove("display");
+}
+
  function createTableData(array) {
-     
+    
     for (let i = 0; i < array.length; i++) {
         table.innerHTML+=  ` <tr class="multipleOpenBtn">
         <td>${array[i].userId}</td>
@@ -22,8 +53,10 @@ console.log(tableHeader);
   }
 
   fetch('https://jsonplaceholder.typicode.com/todos/') // ajax
+  displayLoading()
   .then(response => response.json())
   .then(todos =>  {
+    hideLoading()
     createTableData(todos);
   })
 
@@ -33,3 +66,5 @@ function formInputs(){
 
 
 
+
+  
