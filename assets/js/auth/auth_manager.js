b9aebb6a3df8e8eecf178167f91c834333259ece
Rojhon <rojhonbuenaventura5@gmail.com>
2022-07-07 08:51:13 +0800

    Login Integration

---
+++ assets/js/auth/auth_manager.js
@@ -0,0 +1,89 @@
+import { API_BASE_URL } from "../constants/ApiConstant"
+
+export const login = async (values) => {
+    var xhr = new XMLHttpRequest()
+    var url = `${API_BASE_URL}/api/user/login`
+    var httpMethod = 'POST'
+
+    xhr.open(httpMethod, url, true)
+
+    xhr.setRequestHeader('Content-type', 'application/json')
+
+    xhr.onload = function () {
+        if (this.status == 200) {
+            var data = JSON.parse(this.responseText).split("&")
+
+            if (data[0] == "Success") {
+                var authToken = data[1].split(" ")[1]
+				var accessToken = data[2]
+
+                localStorage.setItem("auth_token", authToken)
+				localStorage.setItem("access_token", accessToken)
+				
+				return window.location.href = "/registrar.html" 
+            } else {
+                alert(data)
+            }
+        } else if (this.status = 404) {
+            alert("Error")
+        }
+    }
+
+    xhr.send(JSON.stringify(values))
+}
+
+export const logout = async (values) => {
+    var xhr = new XMLHttpRequest()
+    var url = `${API_BASE_URL}/api/user/logout`
+    var httpMethod = 'POST'
+
+    xhr.open(httpMethod, url, true)
+
+    xhr.setRequestHeader('Content-type', 'application/json')
+
+    xhr.onload = function () {
+        if (this.status == 200) {
+            var data = JSON.parse(this.responseText)
+
+            if (data == "Success") {
+                localStorage.clear();
+            } else {
+                alert("Error")
+            }
+
+        } else if (this.status = 404) {
+            alert("Error")
+        }
+    }
+
+    xhr.send(JSON.stringify(values))
+}
+
+export const isAuthenticated = () => {
+	try {
+		var accessToken = localStorage.getItem("access_token")
+		var accessTokenDecoded = window.atob(accessToken)
+		var userData = JSON.parse(accessTokenDecoded)
+		
+		if(userData.Authorized)
+			return true
+		
+	} catch (error) {
+		return window.location.href = "/login.html"
+	}	
+}
+
+export const currentUser = () => {
+	try {
+		var accessToken = localStorage.getItem("access_token")
+		var accessTokenDecoded = window.atob(accessToken)
+		var userData = JSON.parse(accessTokenDecoded)
+		
+		delete userData.Authorized
+		
+		return userData
+		
+	} catch (error) {
+		return {}
+	}	
+}
--- assets/js/auth/login.js
+++ assets/js/auth/login.js
@@ -1,4 +1,4 @@
-import { LOCAL_BASE_URL } from "../constants/ApiConstant"
+import { login } from "./auth_manager"
 
 const form = document.getElementById("form-account-login")
 const email = document.getElementById("email")
@@ -13,33 +13,3 @@ form.addEventListener("submit", e => {
 
     login(values)
 })
-
-async function login(values) {
-    var xhr = new XMLHttpRequest()
-    var url = `${LOCAL_BASE_URL}/api/user/login`
-    var httpMethod = 'POST'
-
-    xhr.open(httpMethod, url, true)
-
-    xhr.setRequestHeader('Content-type', 'application/json')
-
-    xhr.onload = function () {
-        if (this.status == 200) {
-            var data = JSON.parse(this.responseText)
-            var res = data.split("&")
-
-            if (res[0] == "Success") {
-                var accessToken = res[1].split(" ")[1]
-
-                localStorage.setItem("access_token", accessToken)
-                alert("Login Success")
-            } else {
-                alert(data)
-            }
-        } else if (this.status = 404) {
-            console.log("error")
-        }
-    }
-
-    xhr.send(JSON.stringify(values))
-}
